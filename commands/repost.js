aec097226a689328dd563b5d1b15ba60bb2e8deb
Rafael Mazzutti <rafaelmazz22@gmail.com>
2023-02-09 22:11:21 -0300

    v4.4.0

---
+++ commands/repost.js
@@ -0,0 +1,24 @@
+const { SlashCommandBuilder } = require('discord.js');
+const fs = require("fs");
+
+let rand = function (min, max)
+{
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName("repost")
+		.setDescription("Manda áudio de repost."),
+	async execute(interaction) {
+        await interaction.deferReply();
+        await fs.readdir("etc/repeat/", async function (err, files)
+		{
+			return await interaction.editReply({
+				files: [
+					`etc/repeat/${files[rand(0, files.length - 1)]}`
+				]
+			});
+		});
+	},
+};
